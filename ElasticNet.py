ef273fbc637a856a6987387d780627738535a512
xbh0403 <xbh0403@gmail.com>
2024-03-06 00:24:36 -0800

    Finished implemented easy models on sklearn

---
+++ ElasticNet.py
@@ -0,0 +1,47 @@
+#%%
+from helper_fns import *
+from tqdm import tqdm
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+def ElasticNet(X, Y, alpha=0.1, l1_ratio=0.5, test_size=0.2, random_state=42):
+    X_std = StandardScaler().fit_transform(X)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_std, Y, test_size=test_size, random_state=random_state)
+    clf = linear_model.ElasticNet(alpha=alpha, l1_ratio=l1_ratio)
+    clf.fit(X_train, Y_train)
+    return {
+        "clf": clf,
+        "X_train": X_train,
+        "X_test": X_test,
+        "Y_train": Y_train,
+        "Y_train_pred": clf.predict(X_train),
+        "Y_test": Y_test,
+        "Y_test_pred": clf.predict(X_test),
+        "r2_train": clf.score(X_train, Y_train),
+        "r2_test": clf.score(X_test, Y_test)
+    }
+
+def ElasticNet_all_genes(protein_genes, y_full_df, ancsetry, alpha=0.1, l1_ratio=0.5, test_size=0.2, random_state=42):
+    results = {}
+    for gene_id in tqdm(protein_genes["gene_id"]):
+        processed_geno, X, Y = process_one_gene(gene_id, protein_genes, ancsetry, y_full_df)
+        results[gene_id] = ElasticNet(X, Y, alpha=alpha, l1_ratio=l1_ratio, test_size=test_size, random_state=random_state)
+    return results
+
+
+if __name__ == "__main__":
+    EUR_ge_regressed, YRI_ge_regressed, EUR_protein_genes, YRI_protein_genes = load_data()
+
+    if not os.path.exists("./project_data/results"):
+        os.makedirs("./project_data/results")
+
+    EUR_results = ElasticNet_all_genes(EUR_protein_genes, EUR_ge_regressed, "EUR")
+    with open("./project_data/results/elasticnet_results.pkl", "wb") as f:
+        pickle.dump(EUR_results, f)
+    print("Results saved in ./project_data/results/elasticnet_results.pkl")
+
+    YRI_results = ElasticNet_all_genes(YRI_protein_genes, YRI_ge_regressed, "YRI")
+    with open("./project_data/results/elasticnet_results.pkl", "wb") as f:
+        pickle.dump(YRI_results, f)
+    print("Results saved in ./project_data/results/elasticnet_results.pkl")
---
+++ helper_fns.py
@@ -0,0 +1,145 @@
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+from typing import List, Tuple, Dict, Any, Optional
+import pickle
+
+os.chdir('/Users/xbh0403/Desktop/TWAS_ASSOC')
+
+
+from pyplink import PyPlink
+
+def load_data():
+    EUR_ge_regressed = pd.read_csv("./project_data/GEUVADIS_EUR_ge_regressed.tsv.gz", sep="\t", index_col=0, compression="gzip")
+    YRI_ge_regressed = pd.read_csv("./project_data/GEUVADIS_YRI_ge_regressed.tsv.gz", sep="\t", index_col=0, compression="gzip")
+
+    EUR_protein_genes = pd.read_csv("./project_data/GEUVADIS_EUR_protein_genes.tsv.gz", sep="\t", index_col=0, compression="gzip")
+    EUR_protein_genes["chr"] = EUR_protein_genes.index
+    EUR_protein_genes.reset_index(drop=True, inplace=True)
+    YRI_protein_genes = pd.read_csv("./project_data/GEUVADIS_YRI_protein_genes.tsv.gz", sep="\t", index_col=0, compression="gzip")
+    YRI_protein_genes["chr"] = YRI_protein_genes.index
+    YRI_protein_genes.reset_index(drop=True, inplace=True)
+
+    print("Shapes of the dataframes:", EUR_ge_regressed.shape, YRI_ge_regressed.shape, EUR_protein_genes.shape, YRI_protein_genes.shape)
+
+    return EUR_ge_regressed, YRI_ge_regressed, EUR_protein_genes, YRI_protein_genes
+
+def find_snps_in_gene(chr_num: int, start: int, end: int, ancsetry: str) -> pd.DataFrame:
+    """
+    Find SNPs within a specified genomic region.
+
+    Parameters:
+    chr_num (int): Chromosome number.
+    start (int): Start position of the genomic region.
+    end (int): End position of the genomic region.
+    bim (pd.DataFrame): DataFrame containing SNP information.
+
+    Returns:
+    pd.DataFrame: DataFrame containing SNPs within the specified genomic region.
+    
+    """
+    with PyPlink("./project_data/geno/"+ancsetry+"/GEUVADIS_"+ancsetry+"_chr"+str(chr_num)) as bed:
+        # Getting the BIM and FAM
+        bim = bed.get_bim()
+        bim["snp"] = bim.index
+        bim.reset_index(drop=True, inplace=True)
+        bim = bim[["snp", "chrom", "pos", "cm", "a1", "a2"]]
+    return bim[(bim["pos"] >= start) & (bim["pos"] <= end)]
+
+def process_geno(ancsetry: str, chr_num: int, snps: List[str], start: int, gene_id: str, save_result: bool = False) -> np.ndarray:
+    """
+    Process the genotype data.
+
+    Parameters:
+    ancsetry (str): Ancestry of the individual.
+    chr_num (int): Chromosome number.
+    individual (str | None): Individual ID.
+    snps (List[str]): List of SNPs.
+
+    Returns:
+    pd.DataFrame: Processed genotype data.
+    """
+    if len(snps) == 0:
+        raise ValueError("No SNPs provided.")
+    
+    with PyPlink("./project_data/geno/"+ancsetry+"/GEUVADIS_"+ancsetry+"_chr"+str(chr_num)) as bed:
+        # Getting the BIM and FAM
+        bim = bed.get_bim()
+        bim["snp"] = bim.index
+        bim.reset_index(drop=True, inplace=True)
+        bim = bim[["snp", "chrom", "pos", "cm", "a1", "a2"]]
+        snp_info = bim[bim["snp"].isin(snps)]
+
+        fam = bed.get_fam()
+        iids = fam["iid"].tolist()
+
+        results = {
+            "snp_info": snp_info,
+            "iids": iids,
+        }
+
+        keep_bool = [1] * len(iids)
+
+        for snp_id, genotypes in bed.iter_geno_marker(snps):
+            results[snp_id] = genotypes
+            genotypes_kept = [0 if x == -1 else 1 for x in genotypes]
+            keep_bool = [x*y for x, y in zip(keep_bool, genotypes_kept)]
+
+        results["keep_bool"] = keep_bool
+
+        for snp_id, genotypes in bed.iter_geno_marker(snps):
+            results[snp_id+"_filtered"] = [x for x, y in zip(genotypes, keep_bool) if y == 1]
+        
+        results["iids_filtered"] = [x for x, y in zip(iids, keep_bool) if y == 1]
+
+        X = np.zeros((len(results["iids_filtered"]), snp_info.shape[0]))
+
+        for i in range(snp_info.shape[0]):
+            X[:, i] = results[snps[i]+"_filtered"]
+
+        if save_result:
+            with open("./project_data/processed_Xy/"+gene_id+"_results.pkl", "wb") as f:
+                pickle.dump(results, f)
+            # save x as npy
+            np.save("./project_data/processed_Xy/X/"+gene_id+"_X.npy", X)
+
+        return results, X
+    
+def get_y(gene_id: str, iids: List[str], y_full_df: pd.DataFrame, save_y: bool = False) -> np.ndarray:
+    """
+    Get the Y values for a specific gene and ancestry.
+
+    Parameters:
+    gene_id (str): Gene ID.
+    iids (List[str]): List of individual IDs.
+    y_full_df (pd.DataFrame): DataFrame containing the Y values.
+
+    Returns:
+    np.ndarray: Array of Y values for the specified gene and ancestry.
+    """
+    all_columns = y_full_df.columns
+    id_gene = all_columns.get_loc(gene_id)
+    all_individuals = y_full_df.index
+    iids_keep = [True if x in iids else False for x in all_individuals]
+    y = y_full_df.iloc[iids_keep, id_gene].values
+    if save_y:
+        np.save("./project_data/processed_Xy/y/"+gene_id+"_y.npy", y)
+    return y
+
+def process_one_gene(gene_id: str, protein_genes: pd.DataFrame, ancsetry: str, y_full_df: pd.DataFrame) -> np.ndarray:
+    gene = protein_genes[protein_genes["gene_id"] == gene_id]
+    assert gene.shape[0] == 1
+    chr_num = gene["chr"].values[0]
+    start = gene["start"].values[0]
+    end = gene["end"].values[0]
+    gene_name = gene["name"].values[0]
+    # print("Gene id: ", gene_id, " Gene name: ", gene_name, " Chr: ", chr_num, " Start: ", start, " End: ", end)
+    
+    snps = find_snps_in_gene(chr_num, start, end, ancsetry)
+    snps_name = snps["snp"].tolist()
+    # print("Number of SNPs: ", len(snps_name))
+    processed_geno, X = process_geno(ancsetry, chr_num, snps_name, start, gene_id, save_result=False)
+    y = get_y(gene_id, processed_geno["iids_filtered"], y_full_df, save_y=False)
+    return processed_geno, X, y
